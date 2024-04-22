36299c31d7acda7f8c205278b7fb656922616228
Reguide Wiki (Blockchain) <127845264+ReguideWIKI@users.noreply.github.com>
2024-04-22 10:32:20 +0700

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+exists()
+{
+  command -v "$1" >/dev/null 2>&1
+}
+if exists curl; then
+echo ''
+else
+  sudo apt update && sudo apt install curl screen -y < "/dev/null"
+fi
+
+# install NVM
+curl -sL https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.0/install.sh -o install_nvm.sh
+sh install_nvm.sh
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+# Installer NodeJS (16.20) requirement
+nvm install 16.20
+nvm use 16.20
+
+# Create Folder
+mkdir ~/p0tion-tmp
+cd ~/p0tion-tmp
